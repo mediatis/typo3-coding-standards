783242a072dd39f297d265d968b8662e7d9eaacd
Stephan Ude <ude@mediatis.de>
2023-09-29 10:20:39 +0200

    [TASK] base implementation

---
+++ src/Php/Typo3CsFixerSetup.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mediatis\Typo3CodingStandards\Php;
+
+use Mediatis\CodingStandards\Php\CsFixerSetup;
+use PhpCsFixer\Config;
+use TYPO3\CodingStandards\CsFixerConfig;
+
+class Typo3CsFixerSetup
+{
+    public static function create(): Config
+    {
+        $config = CsFixerConfig::create();
+        CsFixerSetup::setup($config);
+
+        $config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
+        return $config;
+    }
+}
