772db31bc2f7c125982589aa321f3afbc5c23d2e
Meelis Karulin <karulin@mediatis.de>
2024-04-08 16:15:42 +0200

    Add template to dummy Math class

--- example-extension/Classes/Math.php
+++ example-extension/Classes/Math.php
@@ -6,6 +6,14 @@ namespace ExampleVendor\ExampleExtension;
 
 class Math
 {
+    /**
+     * @template T of int|float
+     *
+     * @param T $x
+     * @param T $y
+     *
+     * @return T
+     */
     public function add(int|float $x, int|float $y): float|int
     {
         return $x + $y;
